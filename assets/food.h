36686dce8acc1d3cfdca3e10e658a46426b2c5fa
sridds <seth.riddensdale@mymail.champlain.edu>
2024-04-15 20:56:09 -0400

    Assets folder setup

---
+++ assets/food.h
@@ -0,0 +1,39 @@
+//
+// Created by Seth Riddensdale on 4/15/24.
+//
+
+#ifndef MYPROJECT_FOOD_H
+#define MYPROJECT_FOOD_H
+
+using namespace std;
+
+class Food {
+public:
+    string
+};
+
+class Produce : Food {
+
+};
+
+class Dairy : Food {
+
+};
+
+class Baking : Food {
+
+};
+
+class Spices : Food {
+
+};
+
+class Deli : Food {
+
+};
+
+class Frozen : Food {
+
+};
+
+#endif //MYPROJECT_FOOD_H
