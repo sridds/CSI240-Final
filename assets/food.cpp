fd57f1c3b19caa706b818d36eb2bb6d98fec5160
sridds <seth.riddensdale@mymail.champlain.edu>
2024-04-15 21:46:58 -0400

    Started Food class setups
    Abstract food classes and some derived food types setup with bare minimum data

--- assets/food.cpp
+++ assets/food.cpp
@@ -3,3 +3,26 @@
 //
 
 #include "food.h"
+#include <sstream>
+
+Food::Food(string name, double price){
+    this->name = name;
+    this->price = price;
+}
+
+string Food::getName() { return name; }
+double Food::getPrice() { return price; }
+
+// Deli
+Deli::Deli(string name, double price, double pounds) : Food(name, price){
+    this->pounds = pounds;
+}
+
+double Deli::getPounds(){
+    return pounds;
+}
+
+string Deli::print() {
+    stringstream stream;
+    stream << name + " ~ $" + to_string(price) + " ~ Pounds: " + to_string(pounds);
+}
--- assets/food.h
+++ assets/food.h
@@ -4,36 +4,58 @@
 
 #ifndef MYPROJECT_FOOD_H
 #define MYPROJECT_FOOD_H
+#include <iostream>
 
 using namespace std;
 
 class Food {
+protected:
+    string name;
+    double price;
+
 public:
-    string
+    // constructors
+    Food(string name, double price);
+
+    // getters
+    string getName();
+    double getPrice();
+
+    // methods
+    virtual string print() = 0;
 };
 
 class Produce : Food {
 
+    string print() override;
 };
 
 class Dairy : Food {
-
+    string print() override;
 };
 
 class Baking : Food {
-
+    string print() override;
 };
 
 class Spices : Food {
-
+    string print() override;
 };
 
 class Deli : Food {
+private:
+    double pounds;
 
+public:
+    Deli(string name, double price, double pounds);
+
+    double getPounds();
+    string print() override;
 };
 
 class Frozen : Food {
 
+    string print() override;
 };
 
 #endif //MYPROJECT_FOOD_H
