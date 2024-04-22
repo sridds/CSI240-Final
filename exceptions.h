2ee46cde3c2d05c729211b133a52b8c25df5dbc2
sridds <seth.riddensdale@mymail.champlain.edu>
2024-04-22 19:34:35 -0400

    Added Headers to files
    Added my headers to my files

--- exceptions.h
+++ exceptions.h
@@ -1,6 +1,29 @@
-//
-// Created by Seth Riddensdale on 4/22/24.
-//
+/*
+Author        : Seth Riddensdale
+Class         : CSI - 240 : Advanced Programming
+Assignment    : Final Project
+Due Date      : 4/23/24 11:59PM
+
+Project Description :
+This project is a grocery game where the user is presented a grocery list and must collect all grocery items from the aisles.
+- The grocery list, and the aisles, are randomly generated based on a file
+- The users user data is stored in a text file, along with statistics for each run of the game
+- The user must navigate through the aisles to find their items.
+- The user's time is recorded as they shop, and they are encouraged to get the best time possible.
+
+
+Certification of Authenticity :
+
+I certify that this is entirely my own work, except where I have given
+fully-documented references to the work of others. I understand the
+definition and consequences of plagiarism and acknowledge that the assessor
+of this assignment may, for the purpose of assessing this assignment:
+- Reproduce this assignment and provide a copy to another member of
+academic staff; and/or
+- Communicate a copy of this assignment to a plagiarism checking
+service (which may then retain a copy of this assignment on its
+database for the purpose of future plagiarism checking)
+*/
 
 #ifndef MYPROJECT_EXCEPTIONS_H
 #define MYPROJECT_EXCEPTIONS_H
